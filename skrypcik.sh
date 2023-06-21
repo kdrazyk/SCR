966f6ec541b6fd86cb8f1ceab65334176ed78a02
Kacper Drazyk <kacper.drazyk@outlook.com>
2023-06-21 19:41:22 +0200

    komentarze do skryptu

--- skrypcik.sh
+++ skrypcik.sh
@@ -28,12 +28,17 @@ MASZYNA_B_STATIC_IP=10.0.2.10
 # ============================================================================ #
 # ============================================================================ #
 
-# Na wszelki wypadek
+# Aktualizacja na wszelki wypadek
 sudo apt update && apt upgrade -y
 
-# DHCP i STATIC
+
+# ============================================================================ #
+#                        Konfiguracja DHCP i static ip
+# ============================================================================ #
+# instalacja serwera DHCP
 sudo apt install isc-dhcp-server -y
 
+# edycja pliku /etc/dhcp/dhcpd.conf
 echo "
 subnet $SIEC_A_IP netmask $SIEC_A_MASKA {
        range $SIEC_A_DHCP_START $SIEC_A_DHCP_STOP;
@@ -55,20 +60,37 @@ host SCR-A {
 }
 " | sudo tee -a /etc/dhcp/dhcpd.conf
 
-# SSH
+
+# ============================================================================ #
+#                                     SSH
+# ============================================================================ #
+# instalacja serwera SSH
 sudo apt install openssh-server -y
 
-# FIREWALL
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow from $MASZYNA_B_STATIC_IP to any port 22
-sudo ufw allow 80
-sudo ufw enable
+# ============================================================================ #
+#                                UFW - FIREWALL
+# ============================================================================ #
+sudo ufw default deny incoming                          # odrzuć połączenia przychodzące
+sudo ufw default allow outgoing                         # przepuść połączenia wychodzące
+sudo ufw allow from $MASZYNA_B_STATIC_IP to any port 22 # zezwól na połączenie SSH z IP maszyny B
+sudo ufw allow 80                                       # zezwól na połączenie ze stroną WWW
+sudo ufw enable                                         # włącz ufw
 
-# MASQUERADE
+# ============================================================================ #
+#                               NAT - MASQUERADE
+# ============================================================================ #
+# podmień
+# DEFAULT_FORWARD_POLICY="DROP"
+# na
+# DEFAULT_FORWARD_POLICY="ACCEPT"
+# w pliku /etc/default/ufw
 sudo sed -i -e "s/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/" /etc/default/ufw
 
+# odkomentuj net/ipv4/ip_forward=1 w pliku /etc/ufw/sysctl.conf
+# (wystarczyłoby usunąć komentarz, ale prościej jest dopisać linijkę na początek pliku)
 echo "net/ipv4/ip_forward=1" | sudo cat - /etc/ufw/sysctl.conf > temp && sudo mv temp /etc/ufw/sysctl.conf
+
+# dopisz konfigurację MASQUERADE na początku pliku /etc/ufw/before.rules
 echo "
 *nat
 :POSTROUTING ACCEPT [0:0]
@@ -78,8 +100,13 @@ COMMIT
 
 " | sudo cat - /etc/ufw/before.rules > temp && sudo mv temp /etc/ufw/before.rules
 
-# SERWER WWW
+# ============================================================================ #
+#                                  SERWER WWW
+# ============================================================================ #
+# pobierz serwer WWW apache2
 sudo apt install apache2 -y
+# podmień domyślną stronę na zwykły tekst
 echo 'Moja strona www' | sudo tee /var/www/html/index.html
 
+# zrestartuj maszynę
 sudo reboot
